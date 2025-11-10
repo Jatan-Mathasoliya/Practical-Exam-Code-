aa22a3a70c443bd0e4f449a49b65b04d3de8603a
Jatan-Mathasoliya <jatan.codinggita@gmail.com>
2025-11-10 11:31:08 +0530

    Adding code of DSA SET-A Practical Exam.

---
+++ DSA_SET_1/Q1_LinkedList.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* next;
+    
+    Node(int val) {
+        data = val;
+        next = nullptr;
+    }
+};
+
+class LinkedList {
+    Node* head;
+
+public:
+    LinkedList() {
+        head = nullptr;
+    }
+
+    void insertAtFront(int val) {
+        Node* newNode = new Node(val);
+        newNode->next = head;
+        head = newNode;
+    }
+
+    void display() {
+        Node* temp = head;
+        while (temp != nullptr) {
+            cout << temp->data << " ";
+            temp = temp->next;
+        }
+        cout << endl;
+    }
+};
+
+int main() {
+    LinkedList list;
+    list.insertAtFront(10);
+    list.insertAtFront(20);
+    list.insertAtFront(30);
+    list.display();
+    return 0;
+}
